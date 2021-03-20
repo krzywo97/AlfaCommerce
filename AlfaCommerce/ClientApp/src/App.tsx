e0c1b079ca2e02a4e6122a2a8eaae5cc84a5b101
Krzysztof Wójcik <krzysztofw.2744@gmail.com>
2021-03-20 20:11:12 +0100

    Added product view for customers Added bootstrap icons as a dependency

--- AlfaCommerce/ClientApp/src/App.tsx
+++ AlfaCommerce/ClientApp/src/App.tsx
@@ -5,6 +5,7 @@ import Admin from './components/admin/Admin'
 import './custom.css'
 import CategoryView from './components/CategoryView'
 import Layout from './components/Layout'
+import ProductView from './components/ProductView'
 
 export default class App extends React.PureComponent {
     render() {
@@ -15,6 +16,10 @@ export default class App extends React.PureComponent {
                 <Route path='/categories/:categoryId' render={props => {
                     let categoryId = props.match.params.categoryId
                     return <CategoryView key={categoryId} id={categoryId}/>
+                }}/>
+                <Route path='/products/:categoryId' render={props => {
+                    let categoryId = props.match.params.categoryId
+                    return <ProductView key={categoryId} id={categoryId}/>
                 }}/>
             </Layout>
         )
