3d5f944f69508865e6ac4659874dcae0406ca253
Krzysztof Wójcik <krzysztofw.2744@gmail.com>
2021-03-31 23:34:12 +0200

    Modify Program.cs to use custom HTTPS certificate, if specified in app settings JSON file

--- AlfaCommerce/Program.cs
+++ AlfaCommerce/Program.cs
@@ -11,6 +11,9 @@ namespace AlfaCommerce
 {
     public class Program
     {
+        private static String CertificateFilename { get; set; }
+        private static String CertificatePassword { get; set; }
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
@@ -18,6 +21,24 @@ namespace AlfaCommerce
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureServices((context, _) =>
+                {
+                    CertificateFilename = context.Configuration["HttpsCertificate:Filename"];
+                    CertificatePassword = context.Configuration["HttpsCertificate:Password"];
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel(o =>
+                    {
+                        if (!string.IsNullOrEmpty(CertificateFilename))
+                        {
+                            o.ListenAnyIP(5001,
+                                options => { options.UseHttps(CertificateFilename, CertificatePassword); });
+                            o.ListenAnyIP(5000);
+                        }
+                    });
+
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
